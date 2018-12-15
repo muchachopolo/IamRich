94a9cd206d2b8d9d867577db00648c8885b6826e
Pablo Daza Perez <muchachopolo@gmail.com>
2018-12-15 12:09:44 -0400

    Initial Commit

---
+++ IamRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IamRich
+//
+//  Created by Pablo Daza on 12/15/18.
+//  Copyright © 2018 Assuresoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IamRichTests/IamRichTests.swift
@@ -0,0 +1,34 @@
+//
+//  IamRichTests.swift
+//  IamRichTests
+//
+//  Created by Pablo Daza on 12/15/18.
+//  Copyright © 2018 Assuresoft. All rights reserved.
+//
+
+import XCTest
+@testable import IamRich
+
+class IamRichTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
